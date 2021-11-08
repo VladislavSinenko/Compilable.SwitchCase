1c390607e4ca1cba441063eb46fc163227810081
VladislavSinenko <synenkovladyslav@gmail.com>
2021-11-08 17:52:52 +0200

    check concurency

--- CompilableClient/Program.cs
+++ CompilableClient/Program.cs
@@ -5,6 +5,8 @@ using Compilable.Extensions;
 using Compilable.Factories;
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace CompilableClient
 {
@@ -12,45 +14,21 @@ namespace CompilableClient
     {
         static void Main(string[] args)
         {
-            ISwitchCaseFactory factory = new SwitchCaseFactory();
-            factory.AddSwitchCase<int, object>(c =>
+            var builder = new SwitchCaseBuilder<int, string>();
+            var range = Enumerable.Range(0, 1000);
+
+            foreach (var item in range)
+                builder.AddSingletonCase(item, item.ToString());
+
+            var provider = builder.GetSwitchCase();
+            var tryGetDigit = provider.GetDelegate();
+
+            var tuples = range.AsParallel().Select(n => { tryGetDigit(n, out string value); return n.ToString() != value; });
+
+            foreach (var item in tuples.Where(t => t))
             {
-                c.AddCase(0, () => new object());
-                c.AddSingletonCase(1, new object());
-                c.SetDefaultAsSingleton("default");
-                return c;
-            }, "getString");
-
-            factory.TryGetProvider(
-                "getString", 
-                out ISwitchCaseProvider<int, object> switchCase);
-
-            factory.TryGetDelegate(
-                "getString", 
-                out TryGetDelegate<int, object> _delegate);
-
-            _delegate(1, out object one);
-            _delegate(int.MinValue, out object minValue);
-
-            Console.WriteLine(one);
-            Console.WriteLine(minValue);
-
-            IEnumerable<KeyValuePair<int, object>> cases = switchCase.AsEnumerable();
-
-            foreach (var _case in cases)
-                Console.WriteLine($"{_case.Key}:{_case.Value}");
-
-            string _default = (string)switchCase.GetDefaultCase();
-
-            Console.WriteLine($"default:{_default ?? "null"}");
-
-            _delegate(0, out var zero1);
-            _delegate(0, out var zero2);
-
-            _delegate(1, out var one2);
-
-            Console.WriteLine(zero1.Equals(zero2));
-            Console.WriteLine(one.Equals(one2));
+                Console.WriteLine(item);
+            }
         }
     }
 }
